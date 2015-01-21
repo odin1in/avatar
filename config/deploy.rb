f5871d45f060a4c39b94933e7ef8baac028abd89
odin1in <odin@pwn.so>
2015-01-21 16:22:11 +0800

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :nginx_server_name, "avatar.pwn.so"
 set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/ckeditor_assets}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
